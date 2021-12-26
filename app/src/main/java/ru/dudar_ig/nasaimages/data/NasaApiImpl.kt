8d25ed7e41f06fd3536ece369358a8c648c945ea
pakad <pakad@mail.ru>
2021-12-26 13:59:09 +0300

    Исправил ключ.

--- app/src/main/java/ru/dudar_ig/nasaimages/data/NasaApiImpl.kt
+++ app/src/main/java/ru/dudar_ig/nasaimages/data/NasaApiImpl.kt
@@ -2,14 +2,12 @@ package ru.dudar_ig.nasaimages.data
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.viewbinding.BuildConfig
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import ru.dudar_ig.nasaimages.BuildConfig.NASA_KEY
-
+import ru.dudar_ig.nasaimages.BuildConfig
 
 private const val BASEURL = "https://api.nasa.gov/"
 
@@ -27,8 +25,7 @@ class NasaApiImpl {
     fun nasaImage(dat: String): LiveData<NasaImage> {
 
         val responseLiveData = MutableLiveData<NasaImage>()
-        //val key = BuildConfig.NASA_KEY
-        api.loadImage("UaRtzzycORXoHSmN7nzJitzMG6LsrOyP6aSLK9Le", dat).enqueue(object : Callback<NasaImage>{
+        api.loadImage(BuildConfig.NASA_KEY_API, dat).enqueue(object : Callback<NasaImage>{
             override fun onResponse(call: Call<NasaImage>, response: Response<NasaImage>) {
                 val jsonImage: NasaImage? = response.body()
                 responseLiveData.value = jsonImage!!
